5e7c1906f0807800b7a068be95915a60ca1ea44c
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-06-06 15:33:08 +0300

    add adaptive parm

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -4,6 +4,7 @@ import {Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
 import Link from "./Link";
 import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
 
 type Props = {
     selectedPage: SelectedPage;
@@ -12,6 +13,8 @@ type Props = {
 
 const Navbar = ({selectedPage, setSelectedPage }: Props) => {
     const flexBetween = "flex items-center justify-between";
+    const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+    const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
 
 
 
@@ -24,6 +27,7 @@ const Navbar = ({selectedPage, setSelectedPage }: Props) => {
                     <img alt="logo" src={Logo}/>
                     
                     {/* Right Side */}
+                    {isAboveMediumScreens ?                    
                     <div className={`${flexBetween} w-full`}>
                         <div className={`${flexBetween} gap-8 text-sm`}>
                             <Link page="Home"
@@ -44,8 +48,19 @@ const Navbar = ({selectedPage, setSelectedPage }: Props) => {
                             <p>Sign In</p>
                             <button>Become a Member</button>
                         </div>
+                    </div> 
+                    : (
+                        <div>
+                            <button
+                            className="rounded-full bg-secondary-500 p-2"
+                            onClick={() => setIsMenuToggled(!isMenuToggled) }>
+                                <Bars3Icon 
+                                className="h-6 w-6 text-white"/>
+                            </button>
 
-                    </div>
+                        </div>
+                    )}
+
                 </div>
             </div>         
         </div>
