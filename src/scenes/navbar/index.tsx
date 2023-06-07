841ba2507b6326008934712a6b3e944a9f25a013
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-06-07 15:48:06 +0300

    add style for navbar

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -10,18 +10,20 @@ import ActionButton from "@/shared/ActionButton";
 type Props = {
     selectedPage: SelectedPage;
     setSelectedPage: (value: SelectedPage ) => void;
+    isTopOfPage: boolean;
 }
 
-const Navbar = ({selectedPage, setSelectedPage }: Props) => {
+const Navbar = ({selectedPage, setSelectedPage, isTopOfPage }: Props) => {
     const flexBetween = "flex items-center justify-between";
     const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
     const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+    const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow";
 
 
 
   return (
     <nav>
-        <div className={`${flexBetween} fixed top-0 z-30 w-full py-6` }>
+        <div className={`${navbarBackground} ${flexBetween} fixed top-0 z-30 w-full py-6` }>
            <div className={`${flexBetween} mx-auto w-5/6`}>
                 <div className={`${flexBetween} w-full gap-16`}>
                     {/* Left Side */}
@@ -78,6 +80,25 @@ const Navbar = ({selectedPage, setSelectedPage }: Props) => {
                     <XMarkIcon className="h-6 w-6 text-grey-400"/>
                 </button>
               </div>
+                {/* MENU ITEMS*/}
+                <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+                    <Link page="Home"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}/>
+                    <Link page="Benefits"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}/>
+                    <Link page="Our Classes"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}/>
+                    <Link page="Contact Us"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}/>
+                </div>
+
+
+
+
 
             </div>
         )}
