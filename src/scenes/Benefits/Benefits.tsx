bd2b92eb597c87259e9d6b640ccf2af8a6e27808
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-06-07 21:31:37 +0300

    add

--- src/scenes/Benefits/Benefits.tsx
+++ src/scenes/Benefits/Benefits.tsx
@@ -103,6 +103,22 @@ function Benefits({setSelectedPage }: Props) {
                     </div>
 
                     {/* DESCRIPTION */}
+                    <div>
+                    <p className="my-5">
+                        Nascetur aenean massa auctor tincidunt. Iaculis potenti amet
+                        egestas ultrices consectetur adipiscing ultricies enim. Pulvinar
+                        fames vitae vitae quis. Quis amet vulputate tincidunt at in
+                        nulla nec. Consequat sed facilisis dui sit egestas ultrices
+                        tellus. Ullamcorper arcu id pretium sapien proin integer nisl.
+                        Felis orci diam odio.
+                    </p>
+                    <p className="mb-5">
+                        Fringilla a sed at suspendisse ut enim volutpat. Rhoncus vel est
+                        tellus quam porttitor. Mauris velit euismod elementum arcu neque
+                        facilisi. Amet semper tortor facilisis metus nibh. Rhoncus sit
+                        enim mattis odio in risus nunc.
+                    </p>
+                    </div>
 
                     {/* BUTTON */}
                 </div>
