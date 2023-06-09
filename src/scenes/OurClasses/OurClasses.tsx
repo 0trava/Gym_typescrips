0b960c98ea2575fa8616867e49b2735ea2f44d0e
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-06-09 18:08:37 +0300

    add CLASS to OurClassws

--- src/scenes/OurClasses/OurClasses.tsx
+++ src/scenes/OurClasses/OurClasses.tsx
@@ -11,7 +11,7 @@ import image5 from "@/assets/image5.png";
 import image6 from "@/assets/image6.png";
 import Class from './Class';
 
-const classes: Array<ClassType> = [
+const classes: Array<any> = [
     {
       name: "Weight Training Classes",
       description:
@@ -77,7 +77,7 @@ const OurClasses = ({setSelectedPage}: Props) => {
             {/* IMAGE-SCROLL */}
             <div className="mt-10 h-[353px] w-full overflow-x-auto overflow-y-hidden">
                 <ul className="w-[2800px] whitespace-nowrap">
-                    {classes.map((item: ClassType, index) => (
+                    {classes.map((item: any, index) => (
                         <Class 
                         key={`${item.name}-${index}`}
                         name={item.name}
