2cb3d0fc88ff4e2a6dc23db5f8c9ea510e4900dc
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-06-09 20:37:57 +0300

    add

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,15 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build:{
-    outDir: "build"
+  build: {
+    rollupOptions: {
+      external: [
+        "react", // ignore react stuff
+        "react-dom",
+      ],
+    },
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   }
-  // resolve: {
-  //   alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
-  // }
 })
