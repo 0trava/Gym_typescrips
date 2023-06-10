eb126542ca2d4d7b9fd3a4a4498460d40c0803c8
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-06-10 16:07:42 +0300

    add

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from "path";
+// import path from "path";
 
 
 // https://vitejs.dev/config/
@@ -14,7 +14,7 @@ export default defineConfig({
       ],
     },
   },
-  resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
-  }
+  // resolve: {
+  //   alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  // }
 })
